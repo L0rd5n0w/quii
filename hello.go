2e4da76946abd730fdafc3ee2217839c48db9c03
L0rd5n0w <ifeoluwaadewusi04@gmail.com>
2026-04-27 04:54:44 +0100

    moreRefactor

--- hello.go
+++ hello.go
@@ -4,15 +4,39 @@ import (
 	"fmt"
 )
 
-const englishHelloPrefix = "Hello, "
+const (
+	yoruba = "Yoruba"
+	french = "French"
+	spanish = "Spanish"
 
-func Hello(name string) string {
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix = "Bonjour, "
+	yorubaHelloprefix = "Kaaro, "
+)
+
+func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	case yoruba:
+		prefix = yorubaHelloprefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello("Chris"))
+	fmt.Println(Hello("Chris", ""))
 }
