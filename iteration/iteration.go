88d47a4af7adee9cb1da7045dddef8b723d4e23b
L0rd5n0w <ifeoluwaadewusi04@gmail.com>
2026-04-28 04:07:23 +0100

    iteration

---
+++ iteration/iteration.go
@@ -0,0 +1,15 @@
+package iteration
+
+import (
+	// "fmt"
+	"strings"
+)
+
+
+func Repeat(character string, times int) string {
+	var repeated strings.Builder
+	for i := 0; i < times; i++ {
+		repeated.WriteString(character)
+	}
+	return repeated.String()
+}
