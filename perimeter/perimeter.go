49bc2fc47d5086e53939d46a25130d335075f818
L0rd5n0w <ifeoluwaadewusi04@gmail.com>
2026-04-29 05:15:30 +0100

    struct,methods&Interfaces

---
+++ perimeter/perimeter.go
@@ -0,0 +1,13 @@
+package perimeter
+
+import (
+
+)
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
+
+func Area(rectangle Rectangle) float64 {
+	return rectangle.Width * rectangle.Height
+}
