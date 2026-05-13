b17e27153b4176b18ef1e6b85769cdd5f9434573
L0rd5n0w <ifeoluwaadewusi04@gmail.com>
2026-05-13 10:39:53 +0100

    refactor

--- blogrender/blogrender.go
+++ blogrender/blogrender.go
@@ -13,6 +13,6 @@ type Post struct {
 }
 
 func Render(w io.Writer, p Post) error {
-	_, err := fmt.Fprintf(w, "<h1>%s</h1>", p.Title)
+	_, err := fmt.Fprintf(w, "<h1>%s</h1><p>%s</p> Tags: <ul><li>%s</li><li>%s</li></ul>", p.Title, p.Description, p.Tags[0], p.Tags[1])
 	return err
 }
