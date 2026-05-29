8fbc836036cf91f65510e58bad296556fa3bc05c
Adewusi Ifeoluwa Daniel <ifeoluwaadewusi04@gmail.com>
2026-05-29 04:32:27 +0100

    time

--- http/CLI.go
+++ http/CLI.go
@@ -4,25 +4,42 @@ import (
 	"bufio"
 	"io"
 	"strings"
+	"time"
 )
 
 // CLI.go
 type CLI struct {
 	playerStore PlayerStore
 	in          *bufio.Scanner
+	alerter		BlindAlerter
 }
 
-func NewCLI(store PlayerStore, in io.Reader) *CLI {
+type BlindAlerter interface {
+	ScheduleAlertAt(duration time.Duration, amount int)
+}
+
+func NewCLI(store PlayerStore, in io.Reader, alerter BlindAlerter) *CLI {
 	return &CLI{
 		playerStore: store,
 		in:          bufio.NewScanner(in),
+		alerter:	 alerter,
 	}
 }
 
 // CLI.go
 func (cli *CLI) PlayPoker() {
+	cli.scheduleBlindAlerts()
 	userInput := cli.readLine()
 	cli.playerStore.RecordWin(extractWinner(userInput))
+}
+
+func(cli *CLI) scheduleBlindAlerts() {
+	blinds := []int{100, 200, 300, 400, 500, 600, 800, 1000, 2000, 4000, 8000}
+	blindTime := 0 * time.Second
+	for _, blind := range blinds {
+		cli.alerter.ScheduleAlertAt(blindTime, blind)
+		blindTime +=	10 * time.Minute
+	}
 }
 
 func extractWinner(userInput string) string {
